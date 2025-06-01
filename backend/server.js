80ef48d4f3a680ccda0c0d589b5de25fbce5d95e
RashmiModi <52698460+RashmiModi@users.noreply.github.com>
2025-06-01 21:23:24 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const connectDB=require("./config/db")
 app.use(
 
     cors({
-        origin:process.env.CLIENT_URL || "*",
+        origin:'https://financialtracker-asxb.vercel.app',
         methods:["GET","POST","PUT","DELETE"],
         allowedHeaders:["Content-Type", "Authorization"]
     })
