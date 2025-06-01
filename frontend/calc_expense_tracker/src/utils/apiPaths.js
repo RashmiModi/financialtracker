90c250d328d795a274d30d4245563a8ce1576572
RashmiModi <52698460+RashmiModi@users.noreply.github.com>
2025-06-01 21:30:13 +0530

    Update apiPaths.js

--- frontend/calc_expense_tracker/src/utils/apiPaths.js
+++ frontend/calc_expense_tracker/src/utils/apiPaths.js
@@ -1,4 +1,4 @@
-export const BASE_URL="https://financialtracker-asxb.vercel.app/"
+export const BASE_URL="https://handsome-illumination-production.up.railway.app"
 
 export const API_PATHS={
     AUTH:{
